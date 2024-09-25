2cb8c339808624d96b49310db4710a543529b34b
tom-walter <tom.k.walter@gmail.com>
2024-09-25 18:43:44 +0200

    Add code on chapter 3, section 5

--- zkp-chaum-pedersen/src/server.rs
+++ zkp-chaum-pedersen/src/server.rs
@@ -4,9 +4,36 @@ pub mod zkp_auth {
     include!("./zkp_auth.rs");
 }
 
-use zkp_auth::auth_server;
+use zkp_auth::{auth_server::{Auth, AuthServer}, AuthenticationAnswerResponse, AuthenticationAnswerRequest, AuthenticationChallengeRequest, AuthenticationChallengeResponse, RegisterRequest, RegisterResponse};
 
+#[derive(Debug, Default)]
+struct AuthImpl { }
 
-fn main() {
-    println!("Hi, I'm a server.");
+#[tonic::async_trait]
+impl Auth for AuthImpl {
+    async fn register(&self, request: Request<RegisterRequest>) -> Result<Response<RegisterResponse>, Status> {
+        todo!()
+    }
+
+    async fn create_authentication_challenge(&self, request: Request<AuthenticationChallengeRequest>) -> Result<Response<AuthenticationChallengeResponse>, Status> {
+        todo!()
+    }
+
+    async fn verify_authentication(&self, request: Request<AuthenticationAnswerRequest>) -> Result<Response<AuthenticationAnswerResponse>, Status> {
+        todo!()
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:50051".to_string();
+    println!("☑ Running the server in {}.", addr);
+
+    let auth_impl = AuthImpl::default();
+
+    Server::builder()
+        .add_service(AuthServer::new(auth_impl))
+        .serve(addr.parse().expect("could not convert address"))
+        .await
+        .unwrap();
 }
